b8b601f4ec952f176595dd5e22e77f72c699b18d
Tristan Rivoallan <tristan@rivoallan.net>
2013-03-13 10:14:41 +0100

    [front] implemented smooth scrolling for nav bar => projects list

--- src/src/ConstructionsIncongrues/ComThibaultHuertasWwwBundle/Resources/public/js/script.js
+++ src/src/ConstructionsIncongrues/ComThibaultHuertasWwwBundle/Resources/public/js/script.js
@@ -1,10 +1,13 @@
-/* Author:
+$(document).ready(function() {
+	// Smooth scrolling for same page links
+	$('div.navbar a').smoothScroll();
 
-*/
+	// Projects covers
+	$('.fade').mosaic();
 
-
-
-
-
-
-
+	// "Caillou" size on home page
+	$('.caillou').height(document.body.clientHeight - 64);
+	$(window).resize(function() {
+		$('.caillou').height(document.body.clientHeight - 64);
+	});
+});
